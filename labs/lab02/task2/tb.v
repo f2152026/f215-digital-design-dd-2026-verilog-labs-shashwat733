cc95f166d904bb7daceb5c51192492a267b09029
Shashwat733 <f20250142@pilani.bits-pilani.ac.in>
2026-09-19 08:45:24 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,27 +1,39 @@
-// tb.v
-// Starter testbench template -- YOU complete this file.
-
 module tb;
+  localparam WIDTH = 8;
+  localparam DEPTH = 4;
 
-  // TODO: declare the inputs and outputs
+  reg [$clog2(DEPTH)-1:0] t_sel;
+  wire [WIDTH-1:0] t_dout;
 
   // TODO: instantiate DUT here
+  lut #(
+    .WIDTH (WIDTH),
+    .DEPTH (DEPTH)
+  ) lut_inst (
+    .sel (t_sel),
+    .dout (t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+      $dumpvars(0, lut_inst);
     end
   end
 
+  integer i;
   initial begin
     // TODO: apply different input combinations
-
+    for (i = 0; i < DEPTH; i = i + 1) begin
+      t_sel = i;
+      #10;
+    end
+    $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%0d dout=%0d", t_sel, t_dout); // change as required
 
 endmodule
